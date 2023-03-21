e1e2aa6b172e822e0d8c4a9d0e4bbf0e4c7a1d7f
Uthpala Liyanage <uthpalaliyanage@yahoo.com>
2023-03-21 11:05:20 +0530

    initial commit

---
+++ src/main/java/com/mrleanage/mockmvctestapp/MockMvcTestAppApplication.java
@@ -0,0 +1,13 @@
+package com.mrleanage.mockmvctestapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MockMvcTestAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MockMvcTestAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mrleanage/mockmvctestapp/MockMvcTestAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mrleanage.mockmvctestapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MockMvcTestAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
